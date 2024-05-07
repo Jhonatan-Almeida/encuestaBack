232d69fbbed74fa99ac1e40418f5454254032d56
Jhonatan Almeida <jhonalmeida@hotmail.com>
2024-05-07 13:46:37 -0500

    Proyecto encuesta

---
+++ src/test/java/com/cds/sistemaencuestas/SistemaEncuestasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cds.sistemaencuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEncuestasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
